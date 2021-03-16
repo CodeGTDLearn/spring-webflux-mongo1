275dd46b479f2b78f0fcf5af9088fb01b33e61c5
SERVIDOR <pvba02@gmail.com>
2021-03-16 17:40:29 -0600

    7.2) Testing API - Test Containers
    Concluded:
    a) Mongo Test Container
    b) Junit 5.0
    c) Application-test.properties is not being read - find the bug
    
    Working on:
    a) Mongo TestContainers (still making this work)

--- src/test/java/com/mongo/AppDriverTests.java
+++ src/test/java/com/mongo/AppDriverTests.java
@@ -1,7 +1,13 @@
 package com.mongo;
 
 import com.mongo.utils.GlobalTestsConfig;
+import org.junit.jupiter.api.Test;
 
 public class AppDriverTests extends GlobalTestsConfig {
+
+    @Test
+    void contextLoads(){
+        System.out.println("Content loads!");
+    }
 
 }
--- src/test/java/com/mongo/utils/GlobalTestsConfig.java
+++ src/test/java/com/mongo/utils/GlobalTestsConfig.java
@@ -4,8 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
@@ -20,39 +23,41 @@ import java.util.concurrent.FutureTask;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
+import static org.springframework.boot.test.context.SpringBootTest.*;
 import static org.springframework.test.annotation.DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD;
 
-//@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
 //@AutoConfigureTestDatabase(replace = Replace.NONE)
 //@TestPropertySource("classpath:config-test.properties")
 //@TestPropertySource("classpath:application-test.properties")
 //@WebFluxTest
-@DirtiesContext(classMode = BEFORE_EACH_TEST_METHOD)
-@ActiveProfiles("test")
-@TestPropertySource(locations = "classpath:application-test.properties")
-@Testcontainers
-@DataMongoTest(excludeAutoConfiguration = EmbeddedMongoAutoConfiguration.class)
-@ExtendWith(SpringExtension.class)
+//@DataMongoTest(excludeAutoConfiguration = EmbeddedMongoAutoConfiguration.class)
+//@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
+//@ExtendWith(SpringExtension.class)
+//@DirtiesContext(classMode = BEFORE_EACH_TEST_METHOD)
+//@ActiveProfiles("test")
+//@Disabled
+//@TestPropertySource(locations = "classpath:application-test.properties")
+//@Testcontainers
+@SpringBootTest
 @Slf4j
-@Disabled
 public class GlobalTestsConfig {
 
     @BeforeAll
     public static void beforeClass() {
-        System.out.println("----------------BEFORECLASS-----------");
+        System.out.println("------GLOBAL----BEFORECLASS-----------");
         BlockHound.install(
                 //builder -> builder.allowBlockingCallsInside("java.util.UUID" ,"randomUUID")
                           );
     }
 
     @AfterAll
-    public static void afterClass()  {
-        System.out.println("----------------AFTERCLASS-------------");
+    public static void afterClass() {
+        System.out.println("------GLOBAL----AFTERCLASS-------------");
     }
 
     @Test
-    public void blockHoundWorks() {
-        System.out.println("------GLOBAL----BLOCKHOUND: START--------");
+    public void bHWorks() {
+        System.out.println("------GLOBAL----BHOUND: START--------");
         try {
             FutureTask<?> task = new FutureTask<>(() -> {
                 Thread.sleep(0);
